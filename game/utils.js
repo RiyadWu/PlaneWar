63346eac239ba1f5532e335da55c24eeb3b48494
cheewu <cheewu@126.com>
2017-08-23 00:16:54 +0800

    fix bug in collide check algorithm

--- game/utils.js
+++ game/utils.js
@@ -18,13 +18,18 @@ const bindAll = (element, selector, eventName, callback) => {
 }
 
 const collide = (a, b) => {
-    if (a.x > b.x && a.x < b.x + b.w) {
-        if (a.y > b.y && a.y < b.y + b.w) {
+    if (aInb(a.x, b.x, b.x + b.w) || aInb(b.x, a.x, a.x + a.w)) {
+        if (aInb(a.y, b.y, b.y + b.h) || aInb(b.y, a.y, a.y + a.h)) {
             return true
         }
     }
     return false
 }
+
+const aInb = (x, x1, x2) => {
+    return x >= x1 && x <= x2
+}
+
 
 const randomNum = (start, end) => {
     const n = Math.random() * (end - start + 1)
